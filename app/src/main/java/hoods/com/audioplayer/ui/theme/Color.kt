9bb69d78dd9c88ab92516fc590c11c8c578c271e
hood <hoods.tech@gmail.com>
2022-03-07 10:51:20 +0300

    Initial Project Setup

---
+++ app/src/main/java/hoods/com/audioplayer/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package hoods.com.audioplayer.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+
+val Blue200 = Color(0xFF30189F)
+val Blue500 = Color(0xFF483974)
+val Blue700 = Color(0xFF1c1348)
+val Orange200 = Color(0xFFeb7603)
+val TextColour = Color(0xFFffffff)
---
+++ app/src/main/java/hoods/com/audioplayer/ui/theme/Theme.kt
@@ -0,0 +1,47 @@
+package hoods.com.audioplayer.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorPalette = darkColors(
+    primary = Purple200,
+    primaryVariant = Purple700,
+    secondary = Teal200
+)
+
+private val LightColorPalette = lightColors(
+    primary = Orange200,
+    primaryVariant = Blue700,
+    secondary = Orange200,
+    background = Blue700,
+    surface = Blue500,
+    onSurface = Color.White,
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun AudioPlayerTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
