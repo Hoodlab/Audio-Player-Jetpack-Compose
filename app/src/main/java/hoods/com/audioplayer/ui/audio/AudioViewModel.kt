fb986155d864e76bd6917ea5f94f1402d1fedd63
hood <hoods.tech@gmail.com>
2022-03-17 08:26:51 +0300

    Created viewModel for the audio player UI

---
+++ app/src/main/java/hoods/com/audioplayer/ui/audio/AudioViewModel.kt
@@ -0,0 +1,197 @@
+package hoods.com.audioplayer.ui.audio
+
+import android.support.v4.media.MediaBrowserCompat
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import hoods.com.audioplayer.data.model.Audio
+import hoods.com.audioplayer.data.repository.AudioRepository
+import hoods.com.audioplayer.media.constants.K
+import hoods.com.audioplayer.media.exoplayer.MediaPlayerServiceConnection
+import hoods.com.audioplayer.media.exoplayer.currentPosition
+import hoods.com.audioplayer.media.exoplayer.isPlaying
+import hoods.com.audioplayer.media.service.MediaPlayerService
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class AudioViewModel @Inject constructor(
+    private val repository: AudioRepository,
+    serviceConnection: MediaPlayerServiceConnection
+) : ViewModel() {
+    var audioList = mutableStateListOf<Audio>()
+    val currentPlayingAudio = serviceConnection.currentPlayingAudio
+    private val isConnected = serviceConnection.isConnected
+    lateinit var rootMediaId: String
+    var currentPlayBackPosition by mutableStateOf(0L)
+    private var updatePosition = true
+    private val playbackState = serviceConnection.plaBackState
+    val isAudioPlaying: Boolean
+        get() = playbackState.value?.isPlaying == true
+    private val subscriptionCallback = object
+        : MediaBrowserCompat.SubscriptionCallback() {
+        override fun onChildrenLoaded(
+            parentId: String,
+            children: MutableList<MediaBrowserCompat.MediaItem>
+        ) {
+            super.onChildrenLoaded(parentId, children)
+        }
+    }
+    private val serviceConnection = serviceConnection.also {
+
+    }
+    val currentDuration = MediaPlayerService.currentDuration
+
+    var currentAudioProgress = mutableStateOf(0f)
+
+    init {
+        viewModelScope.launch {
+            audioList += getAndFormatAudioData()
+            isConnected.collect {
+                if (it) {
+                    rootMediaId = serviceConnection.rootMediaId
+                    serviceConnection.plaBackState.value?.apply {
+                        currentPlayBackPosition = position
+                    }
+                    serviceConnection.subscribe(rootMediaId, subscriptionCallback)
+
+                }
+
+            }
+
+
+        }
+
+
+    }
+
+    private suspend fun getAndFormatAudioData(): List<Audio> {
+        return repository.getAudioData().map {
+            val displayName = it.displayName.substringBefore(".")
+            val artist = if (it.artist.contains("<unknown>"))
+                "Unknown Artist" else it.artist
+            it.copy(
+                displayName = displayName,
+                artist = artist
+
+            )
+        }
+
+
+    }
+
+    fun playAudio(currentAudio: Audio) {
+        serviceConnection.playAudio(audioList)
+        if (currentAudio.id == currentPlayingAudio.value?.id) {
+            if (isAudioPlaying) {
+                serviceConnection.transportControl.pause()
+            } else {
+                serviceConnection.transportControl.play()
+            }
+
+
+        } else {
+            serviceConnection.transportControl
+                .playFromMediaId(
+                    currentAudio.id.toString(),
+                    null
+                )
+        }
+
+
+    }
+
+    fun stopPlayBack() {
+        serviceConnection.transportControl.stop()
+    }
+
+    fun fastForward() {
+        serviceConnection.fastForward()
+    }
+
+    fun rewind() {
+        serviceConnection.rewind()
+    }
+
+    fun skipToNext() {
+        serviceConnection.skipToNext()
+    }
+
+    fun seekTo(value: Float) {
+        serviceConnection.transportControl.seekTo(
+            (currentDuration * value / 100f).toLong()
+        )
+    }
+
+    private fun updatePlayBack() {
+        viewModelScope.launch {
+            val position = playbackState.value?.currentPosition ?: 0
+
+            if (currentPlayBackPosition != position) {
+                currentPlayBackPosition = position
+            }
+
+            if (currentDuration > 0) {
+                currentAudioProgress.value = (
+                        currentPlayBackPosition.toFloat()
+                                / currentDuration.toFloat() * 100f
+
+                        )
+            }
+
+            delay(K.PLAYBACK_UPDATE_INTERVAL)
+            if (updatePosition) {
+                updatePlayBack()
+            }
+
+
+        }
+
+
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        serviceConnection.unSubscribe(
+            K.MEDIA_ROOT_ID,
+            object : MediaBrowserCompat.SubscriptionCallback() {}
+        )
+        updatePosition = false
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
